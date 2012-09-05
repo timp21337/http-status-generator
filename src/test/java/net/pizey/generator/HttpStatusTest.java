ba6aeb837c79e4b36b03dae449f9a32c3adc0a6c
timp21337 <timp@paneris.org>
2012-09-05 01:17:27 +0100

    input size

--- src/test/java/net/pizey/generator/HttpStatusTest.java
+++ src/test/java/net/pizey/generator/HttpStatusTest.java
@@ -7,7 +7,7 @@ public class HttpStatusTest extends TestCase {
   public void testGetPage() {
     assertEquals("<html><head><title>Htpp Status Generator</title>\n"
         + "</head>\n" + "<body><h1>Htpp Status Generator</h1>\n"
-        + "<form>Status <input value=\"200\" width=\"5\" name=\"status\" /></form>\n"
+        + "<form>Status <input value=\"200\" size=\"5\" name=\"status\" /></form>\n"
         + "<p><a href='https://github.com/timp21337/http-status-generator'>github</a></p>\n"
         + "</body>\n" + "</html>\n", new HttpStatus().getPage("200"));
   }
