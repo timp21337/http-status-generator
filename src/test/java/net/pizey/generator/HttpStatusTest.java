193dec29bf47ae3324b7658551c4f78fbaa0e0dc
timp21337 <timp@paneris.org>
2012-09-05 01:05:40 +0100

    Now with width

--- src/test/java/net/pizey/generator/HttpStatusTest.java
+++ src/test/java/net/pizey/generator/HttpStatusTest.java
@@ -7,7 +7,7 @@ public class HttpStatusTest extends TestCase {
   public void testGetPage() {
     assertEquals("<html><head><title>Htpp Status Generator</title>\n"
         + "</head>\n" + "<body><h1>Htpp Status Generator</h1>\n"
-        + "<form>Status <input value=\"200\" name=\"status\" /></form>\n"
+        + "<form>Status <input value=\"200\" width=\"5\" name=\"status\" /></form>\n"
         + "</body>\n" + "</html>\n", new HttpStatus().getPage("200"));
   }
 
