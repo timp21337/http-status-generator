6a5a9d18511fbcd5e23f0c6b320740e419bb1071
timp21337 <timp@paneris.org>
2012-09-05 10:34:39 +0100

    Typo

--- src/test/java/net/pizey/generator/HttpStatusTest.java
+++ src/test/java/net/pizey/generator/HttpStatusTest.java
@@ -5,8 +5,8 @@ import junit.framework.TestCase;
 public class HttpStatusTest extends TestCase {
 
   public void testGetPage() {
-    assertEquals("<html><head><title>Htpp Status Generator</title>\n"
-        + "</head>\n" + "<body><h1>Htpp Status Generator</h1>\n"
+    assertEquals("<html><head><title>Http Status Generator</title>\n"
+        + "</head>\n" + "<body><h1>Http Status Generator</h1>\n"
         + "<form>Status <input value=\"200\" size=\"5\" name=\"status\" /></form>\n"
         + "<p><a href='https://github.com/timp21337/http-status-generator'>github</a></p>\n"
         + "</body>\n" + "</html>\n", new HttpStatus().getPage("200"));
